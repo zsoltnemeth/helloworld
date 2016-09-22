d88c9073cfb7e42b2eb73d1496f72d07538cc5d4
Nemeth Zsolt <zsolt.nemeth@everexfx.com>
2016-09-22 13:29:23 +0200

    asdas

--- helloworld/helloworld/Program.cs
+++ helloworld/helloworld/Program.cs
@@ -11,7 +11,7 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World2");
             Thread.Sleep(2000);
         }
     }
